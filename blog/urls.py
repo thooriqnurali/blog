d982043c73f45b2bd39af4409baa5ac0b144afc1
thooriqnurali <thooriq.na@gmail.com>
2024-03-14 12:32:41 +0700

    add markdown body, sitemaps, and rss feeds feature

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostFeed 
 
 app_name = 'blog'
 
@@ -14,5 +15,6 @@ urlpatterns = [
          views.post_share, name='post_share'),
     path('<int:post_id>/comment/',
          views.post_comment, name='post_comment'),
+    path('feed/', LatestPostFeed(), name='post_feed'),
 ]
 
